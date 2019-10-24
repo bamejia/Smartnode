9b64313d8f15496b7338ae5680504e7aa567eaba
bamejia <bamejia@udel.edu>
2019-10-24 10:15:39 -0400

    Added a second button, one is for sending information, the other is for receiving information.  Send button successfully sends class object to firebase.

--- app/src/main/java/com/example/smartnode/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/smartnode/ui/home/HomeFragment.java
@@ -15,10 +15,24 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.smartnode.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Exclude;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.IgnoreExtraProperties;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+
+    private DatabaseReference mDatabase;
+
+    private Post p = new Post();
+
+    private ValueEventListener tmpListener;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -33,16 +47,105 @@ public class HomeFragment extends Fragment {
             }
         });
 
+//        tmpListener = new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                // Get Post object and use the values to update the UI
+//                Post post = dataSnapshot.getValue(Post.class);
+//                Toast.makeText(getActivity().getApplicationContext(), post.body, Toast.LENGTH_LONG)
+//                        .show();
+//                // ...
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//                // Getting Post failed, log a message
+//                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+//                // ...
+//            }
+//        };
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
         Button sendBtn = root.findViewById(R.id.sendButton);
         sendBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 //                Log.i("Hello There", "Sending info");
-                Toast.makeText(getActivity().getApplicationContext(), "Hello There!", Toast.LENGTH_SHORT)
+
+                p.writeNewPost("101", "bamejia", "hello", "This is a test message");
+
+                Toast.makeText(getActivity().getApplicationContext(), "Message Sent!", Toast.LENGTH_SHORT)
                         .show();
+
+            }
+        });
+
+        Button receiveBtn = root.findViewById(R.id.receiveButton);
+        receiveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Log.i("Hello There", "Sending info");
+
+//                p.writeNewPost("100", "bamejia", "hello", "This is a test message");
+//                mDatabase.addListenerForSingleValueEvent(tmpListener);
+
+                Toast.makeText(getActivity().getApplicationContext(), "Message Received!", Toast.LENGTH_SHORT)
+                        .show();
+
             }
         });
 
         return root;
+    }
+
+    @IgnoreExtraProperties
+    public class Post {
+
+        private String uid;
+        private String author;
+        private String title;
+        private String body;
+        private int starCount = 0;
+        private Map<String, Boolean> stars = new HashMap<>();
+
+        public Post() {
+            // Default constructor required for calls to DataSnapshot.getValue(Post.class)
+        }
+
+        public Post(String uid, String author, String title, String body) {
+            this.uid = uid;
+            this.author = author;
+            this.title = title;
+            this.body = body;
+        }
+
+        @Exclude
+        public Map<String, Object> toMap() {
+            HashMap<String, Object> result = new HashMap<>();
+            result.put("uid", uid);
+            result.put("author", author);
+            result.put("title", title);
+            result.put("body", body);
+            result.put("starCount", starCount);
+            result.put("stars", stars);
+
+            return result;
+        }
+
+        private void writeNewPost(String userId, String username, String title, String body) {
+            // Create new post at /user-posts/$userid/$postid and at
+            // /posts/$postid simultaneously
+            String key = mDatabase.child("posts").push().getKey();
+            Post post = new Post(userId, username, title, body);
+            Map<String, Object> postValues = post.toMap();
+
+            Map<String, Object> childUpdates = new HashMap<>();
+            childUpdates.put("/posts/" + key, postValues);
+            childUpdates.put("/user-posts/" + userId + "/" + key, postValues);
+
+            mDatabase.updateChildren(childUpdates);
+        }
+
     }
 }
